136a67b7a1658995b2175e22cd325d3d184dec47
AfonsoAntunes <afonsorgantunes@gmail.com>
2021-07-02 23:41:41 +0100

    Created functions to insert in the tables

--- app/src/androidTest/java/com/example/safetakeaway/TestDatabase.kt
+++ app/src/androidTest/java/com/example/safetakeaway/TestDatabase.kt
@@ -19,6 +19,42 @@ class TestDatabase {
     private fun getAppContext() = InstrumentationRegistry.getInstrumentation().targetContext
     private fun getDbTakeAwayOpenHelper() = DbTakeAwayOpenHelper(getAppContext())
 
+    private fun insertRestaurant(restaurantTable: RestaurantTable, restaurant: Restaurant): Long {
+        val id = restaurantTable.insert(restaurant.toContentValues())
+        assertNotEquals(-1, id)
+        return id
+    }
+
+    private fun insertPlates(platesTable: PlatesTable, plates: Plates): Long {
+        val id = platesTable.insert(plates.toContentValues())
+        assertNotEquals(-1, id)
+        return id
+    }
+
+    private fun insertOrder(orderTable: OrderTable, order: Order): Long {
+        val id = orderTable.insert(order.toContentValues())
+        assertNotEquals(-1, id)
+        return id
+    }
+
+    private fun insertUser(userTable: UserTable, user: User): Long {
+        val id = userTable.insert(user.toContentValues())
+        assertNotEquals(-1, id)
+        return id
+    }
+
+    private fun insertCategory(categoryTable: CategoryTable, category: Category): Long {
+        val id = categoryTable.insert(category.toContentValues())
+        assertNotEquals(-1, id)
+        return id
+    }
+
+    private fun insertCity(cityTable: CityTable, city: City): Long {
+        val id = cityTable.insert(city.toContentValues())
+        assertNotEquals(-1, id)
+        return id
+    }
+
     @Before
     fun deleteDb() {
         getAppContext().deleteDatabase(DbTakeAwayOpenHelper.DATABASE_NAME)
