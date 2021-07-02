1b8f69a904db74dc459b44207e1e38272a0c1b8e
AfonsoAntunes <afonsorgantunes@gmail.com>
2021-07-03 00:37:56 +0100

    ContentProvider onCreate function implemented

---
+++ app/src/main/java/com/example/safetakeaway/ContentProviderTakeAway.kt
@@ -0,0 +1,46 @@
+package com.example.safetakeaway
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+class ContentProviderTakeAway : ContentProvider() {
+    private var dbTakeAwayOpenHelper : DbTakeAwayOpenHelper? = null
+    override fun onCreate(): Boolean {
+        dbTakeAwayOpenHelper = DbTakeAwayOpenHelper(context)
+
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+        TODO("Not yet implemented")
+    }
+}
