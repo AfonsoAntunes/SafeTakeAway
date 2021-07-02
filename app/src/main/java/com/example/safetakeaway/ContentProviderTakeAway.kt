37bb178efe403b516fb181b96b8318318c493dfe
AfonsoAntunes <afonsorgantunes@gmail.com>
2021-07-03 00:40:08 +0100

    Create constants for each address

--- app/src/main/java/com/example/safetakeaway/ContentProviderTakeAway.kt
+++ app/src/main/java/com/example/safetakeaway/ContentProviderTakeAway.kt
@@ -55,23 +55,36 @@ class ContentProviderTakeAway : ContentProvider() {
         private const val RESTAURANT = "restaurant"
         private const val CATEGORY = "category"
 
+        private const val ORDER_URI = 100;
+        private const val SPECIFIC_ORDER_URI = 101;
+        private const val USER_URI = 200;
+        private const val SPECIFIC_USER_URI = 201;
+        private const val CITY_URI = 300;
+        private const val SPECIFIC_CITY_URI = 301;
+        private const val PLATES_URI = 400;
+        private const val SPECIFIC_PLATES_URI = 401;
+        private const val RESTAURANT_URI = 500;
+        private const val SPECIFIC_RESTAURANT_URI = 501;
+        private const val CATEGORY_URI = 600;
+        private const val SPECIFIC_CATEGORY_URI = 601;
+
         private val BASE_ADDRESS = Uri.parse("content://$AUTHORITY")
 
         private fun getUriMatcher() : UriMatcher {
             val uriMatcher = UriMatcher(UriMatcher.NO_MATCH)
 
-            uriMatcher.addURI(AUTHORITY, ORDER, 100)
-            uriMatcher.addURI(AUTHORITY, "$ORDER/#", 101)
-            uriMatcher.addURI(AUTHORITY, USER, 200)
-            uriMatcher.addURI(AUTHORITY, "$USER/#", 201)
-            uriMatcher.addURI(AUTHORITY, CITY, 300)
-            uriMatcher.addURI(AUTHORITY, "$CITY/#", 301)
-            uriMatcher.addURI(AUTHORITY, PLATES, 400)
-            uriMatcher.addURI(AUTHORITY, "$PLATES/#", 401)
-            uriMatcher.addURI(AUTHORITY, RESTAURANT, 500)
-            uriMatcher.addURI(AUTHORITY, "$RESTAURANT/#", 501)
-            uriMatcher.addURI(AUTHORITY, CATEGORY, 600)
-            uriMatcher.addURI(AUTHORITY, "$CATEGORY/#", 601)
+            uriMatcher.addURI(AUTHORITY, ORDER, ORDER_URI)
+            uriMatcher.addURI(AUTHORITY, "$ORDER/#", SPECIFIC_ORDER_URI)
+            uriMatcher.addURI(AUTHORITY, USER, USER_URI)
+            uriMatcher.addURI(AUTHORITY, "$USER/#", SPECIFIC_USER_URI)
+            uriMatcher.addURI(AUTHORITY, CITY, CITY_URI)
+            uriMatcher.addURI(AUTHORITY, "$CITY/#", SPECIFIC_CITY_URI)
+            uriMatcher.addURI(AUTHORITY, PLATES, PLATES_URI)
+            uriMatcher.addURI(AUTHORITY, "$PLATES/#", SPECIFIC_PLATES_URI)
+            uriMatcher.addURI(AUTHORITY, RESTAURANT, RESTAURANT_URI)
+            uriMatcher.addURI(AUTHORITY, "$RESTAURANT/#", SPECIFIC_RESTAURANT_URI)
+            uriMatcher.addURI(AUTHORITY, CATEGORY, CATEGORY_URI)
+            uriMatcher.addURI(AUTHORITY, "$CATEGORY/#", SPECIFIC_CATEGORY_URI)
 
             return uriMatcher
         }
