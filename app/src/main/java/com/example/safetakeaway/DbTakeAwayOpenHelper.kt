bc296ee8ea00d8cd5a1aa22d0299a34c4526b8f6
AfonsoAntunes <afonsorgantunes@gmail.com>
2021-07-02 22:49:27 +0100

    Create Open Helper

---
+++ app/src/main/java/com/example/safetakeaway/DbTakeAwayOpenHelper.kt
@@ -0,0 +1,20 @@
+package com.example.safetakeaway
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DbTakeAwayOpenHelper (context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }
+
+    companion object {
+        const val DATABASE_NAME = "takeAway.db"
+        const val DATABASE_VERSION = 1
+    }
+}
