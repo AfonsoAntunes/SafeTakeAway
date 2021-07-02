bb78a4dbdbc2690e3dea642010991a6f964eee9f
AfonsoAntunes <afonsorgantunes@gmail.com>
2021-07-03 00:39:08 +0100

    Created uriMatcher to differentiate addresses

--- app/src/main/java/com/example/safetakeaway/ContentProviderTakeAway.kt
+++ app/src/main/java/com/example/safetakeaway/ContentProviderTakeAway.kt
@@ -2,6 +2,7 @@ package com.example.safetakeaway
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 
@@ -42,5 +43,37 @@ class ContentProviderTakeAway : ContentProvider() {
         selectionArgs: Array<out String>?
     ): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val AUTHORITY = "com.example.covidfinalproject"
+
+        private const val ORDER = "order"
+        private const val USER = "user"
+        private const val CITY = "city"
+        private const val PLATES = "plates"
+        private const val RESTAURANT = "restaurant"
+        private const val CATEGORY = "category"
+
+        private val BASE_ADDRESS = Uri.parse("content://$AUTHORITY")
+
+        private fun getUriMatcher() : UriMatcher {
+            val uriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+
+            uriMatcher.addURI(AUTHORITY, ORDER, 100)
+            uriMatcher.addURI(AUTHORITY, "$ORDER/#", 101)
+            uriMatcher.addURI(AUTHORITY, USER, 200)
+            uriMatcher.addURI(AUTHORITY, "$USER/#", 201)
+            uriMatcher.addURI(AUTHORITY, CITY, 300)
+            uriMatcher.addURI(AUTHORITY, "$CITY/#", 301)
+            uriMatcher.addURI(AUTHORITY, PLATES, 400)
+            uriMatcher.addURI(AUTHORITY, "$PLATES/#", 401)
+            uriMatcher.addURI(AUTHORITY, RESTAURANT, 500)
+            uriMatcher.addURI(AUTHORITY, "$RESTAURANT/#", 501)
+            uriMatcher.addURI(AUTHORITY, CATEGORY, 600)
+            uriMatcher.addURI(AUTHORITY, "$CATEGORY/#", 601)
+
+            return uriMatcher
+        }
     }
 }
