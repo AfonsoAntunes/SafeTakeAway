0315d027cdede0d8fc2c7d086a4524e5674aeaf7
AfonsoAntunes <afonsorgantunes@gmail.com>
2021-07-04 23:44:27 +0100

    Adapter created to call RecycleView

---
+++ app/src/main/java/com/example/safetakeaway/AdapterRestaurant.kt
@@ -0,0 +1,24 @@
+package com.example.safetakeaway
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.safetakeaway.AdapterRestaurant.ViewHolderRestaurant
+
+class AdapterRestaurant : RecyclerView.Adapter<ViewHolderRestaurant>() {
+    class ViewHolderRestaurant (itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderRestaurant {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderRestaurant, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
